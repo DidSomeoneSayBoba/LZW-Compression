91ec87ee89eb3460506f603f1586b110fe2a3be0
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2021-09-05 15:40:27 -0700

    i have no idea what im doing lol

--- src/LZW.java
+++ src/LZW.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 //ascii are 8bits, make slightly bigger table, add own characters, save multiple chars
@@ -7,24 +8,41 @@ import java.util.HashMap;
 //if not, write to dict
 // write out current shymbol to msg
 public class LZW {
-	HashMap <Integer, String> dict;
-	Integer[] stringyboi;
+	HashMap <String, Integer> dict;
+	ArrayList<Integer> stringyboi;
 	String curr;
-	
+	char nxt;
 	public LZW()
 	{
 		for(char ch = 'a';ch<='z';ch++)
 		{
-			dict.put((int) ch, ""+ch);
+			dict.put(""+ch,(int) ch);
 		}
 		
 	}
 	public void encode(String input,String outputfile)
 	{
+		int index=0;
+		while(stringyboi.size()<=input.length())
+		{
+			nxt = input.charAt(index);
+			if(curr!=null)
+			{
+				
+			
+			if(dict.containsKey(curr))
+			{
+				stringyboi.add(dict.get(curr));
+				
+			}
+			curr = curr + nxt;
+			}
+			index++;
+		}
 		for(int a=0;a<input.length();a++)
 		{
 			curr = ""+input.charAt(a);
-			if(dict.containsValue(curr)) {
+			if(dict.containsKey(curr)) {
 				
 			}
 		}
